0b94eb97f3b8824bb53a08a195feb16ada532d2e
Elliott Etzkorn <mail@elliottetzkorn.com>
2024-06-25 17:30:45 -0400

    Start app with input loaded

--- lib/components/add_ping_button.dart
+++ lib/components/add_ping_button.dart
@@ -2,115 +2,15 @@ import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter_phosphor_icons/flutter_phosphor_icons.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
-import 'package:practice/constants.dart';
-import 'package:practice/providers/hide_home_screen_provider.dart';
-import 'package:practice/providers/pings_provider.dart';
+import 'package:practice/utils/sheets.dart';
 
 class AddPingButton extends ConsumerWidget {
-  void submitPing(BuildContext context, WidgetRef ref,
-      TextEditingController controller, FocusNode focusNode) {
-    if (controller.text.isNotEmpty) {
-      HapticFeedback.lightImpact();
-
-      ref.read(pingsProvider.notifier).addPing(controller.text);
-      controller.clear();
-      FocusScope.of(context).requestFocus(focusNode);
-    }
-  }
-
   @override
   Widget build(BuildContext context, WidgetRef ref) {
-    final controller = TextEditingController();
-    final focusNode = FocusNode();
-
     return FloatingActionButton(
         onPressed: () {
           HapticFeedback.selectionClick();
-          ref.read(hideHomeScreenProviderProvider.notifier).toggle();
-
-          showModalBottomSheet(
-            backgroundColor: Theme.of(context).scaffoldBackgroundColor,
-            shape: RoundedRectangleBorder(
-              borderRadius: BorderRadius.only(
-                topLeft: Radius.circular(spacingFive),
-                topRight: Radius.circular(spacingFive),
-              ),
-            ),
-            context: context,
-            isScrollControlled: true,
-            builder: (context) {
-              return Stack(children: [
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.end,
-                  children: [
-                    GestureDetector(
-                      behavior: HitTestBehavior.opaque,
-                      onTap: () {
-                        HapticFeedback.selectionClick();
-                        Navigator.pop(context);
-                      },
-                      child: SizedBox(
-                          height: tapTarget,
-                          width: tapTarget,
-                          child: Center(
-                              child: Icon(
-                            PhosphorIcons.x_thin,
-                            color: Theme.of(context).colorScheme.primary,
-                            size: smallX,
-                          ))),
-                    )
-                  ],
-                ),
-                Padding(
-                  padding: EdgeInsets.only(
-                    bottom: MediaQuery.of(context).viewInsets.bottom,
-                    left: spacingFour,
-                    right: spacingFour,
-                    top: spacingFour,
-                  ),
-                  child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.end,
-                    mainAxisSize: MainAxisSize.min,
-                    children: <Widget>[
-                      Row(
-                          mainAxisAlignment: MainAxisAlignment.start,
-                          children: [
-                            Text(
-                              "metadata goes here..",
-                              style: TextStyle(
-                                  color:
-                                      Theme.of(context).colorScheme.secondary),
-                            )
-                          ]),
-                      TextField(
-                        minLines: 1,
-                        maxLines: 6,
-                        controller: controller,
-                        focusNode: focusNode,
-                        textInputAction: TextInputAction.newline,
-                        onSubmitted: (value) {
-                          submitPing(context, ref, controller, focusNode);
-                        },
-                        autofocus: true,
-                        decoration: InputDecoration(
-                          labelText: 'ping',
-                        ),
-                      ),
-                      TextButton(
-                        child: Text('done'),
-                        onPressed: () {
-                          submitPing(context, ref, controller, focusNode);
-                        },
-                      ),
-                    ],
-                  ),
-                )
-              ]);
-            },
-          ).then((value) {
-            ref.read(hideHomeScreenProviderProvider.notifier).toggle();
-          });
-          ;
+          Sheets.showAddPing(context, ref);
         },
         child: Icon(PhosphorIcons.plus));
   }
--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -3,13 +3,58 @@ import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:practice/components/ping_list.dart';
 import 'package:practice/constants.dart';
 import 'package:practice/providers/hide_home_screen_provider.dart';
+import 'package:practice/utils/sheets.dart';
 
-class Home extends ConsumerWidget {
+class Home extends ConsumerStatefulWidget {
+  const Home({super.key});
+
   @override
-  Widget build(BuildContext context, WidgetRef ref) {
+  ConsumerState<Home> createState() => _HomePageState();
+}
+
+class _HomePageState extends ConsumerState<Home> with WidgetsBindingObserver {
+  @override
+  void initState() {
+    super.initState();
+    WidgetsBinding.instance.addObserver(this);
+
+    WidgetsBinding.instance.addPostFrameCallback((_) {
+      if (!ref.read(hideHomeScreenProviderProvider)) {
+        Sheets.showAddPing(context, ref);
+      }
+    });
+  }
+
+  @override
+  void dispose() {
+    WidgetsBinding.instance.removeObserver(this);
+    super.dispose();
+  }
+
+  @override
+  void didChangeAppLifecycleState(AppLifecycleState state) {
+    switch (state) {
+      case AppLifecycleState.resumed:
+        if (!ref.read(hideHomeScreenProviderProvider)) {
+          Sheets.showAddPing(context, ref);
+        }
+        break;
+      case AppLifecycleState.inactive:
+        break;
+      case AppLifecycleState.paused:
+        break;
+      case AppLifecycleState.detached:
+        break;
+      case AppLifecycleState.hidden:
+        break;
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
     return Stack(
       children: [
-        PingList() ,
+        PingList(),
         IgnorePointer(
             ignoring: true,
             child: AnimatedOpacity(
