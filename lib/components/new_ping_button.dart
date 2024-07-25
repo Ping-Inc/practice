6b665daf9ba791c354cac1575948f15c8c2e4c04
Elliott Etzkorn <mail@elliottetzkorn.com>
2024-07-24 23:51:12 -0400

    A bit more chill

--- lib/components/new_ping_button.dart
+++ lib/components/new_ping_button.dart
@@ -25,7 +25,7 @@ class _PingEntryState extends ConsumerState<NewPingButton>
   void initState() {
     super.initState();
     _controller = AnimationController(
-      duration: const Duration(milliseconds: 66),
+      duration: const Duration(milliseconds: 70),
       vsync: this,
     );
     _animation = Tween<double>(begin: 50, end: 44).animate(_controller)
@@ -56,7 +56,7 @@ class _PingEntryState extends ConsumerState<NewPingButton>
                   .read(pingsProvider.notifier)
                   .addPing(widget.textEditingController.text);
               ref.read(currentPingProvider.notifier).reset();
-              Timer(const Duration(milliseconds: 120), () {
+              Timer(const Duration(milliseconds: 140), () {
                 HapticFeedback.selectionClick();
               });
             },
