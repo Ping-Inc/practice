5e7e7ca9e19a197275ae18e8af06da7c0964903f
Elliott Etzkorn <mail@elliottetzkorn.com>
2024-09-04 16:18:17 -0400

    Functional home page

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,94 +1,139 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_phosphor_icons/flutter_phosphor_icons.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
-import 'package:practice/components/bottom_nav.dart';
 import 'package:practice/components/system_tap.dart';
 import 'package:practice/constants.dart';
-import 'package:practice/pages/explore_page.dart';
+import 'package:practice/enums/filters_enum.dart';
+import 'package:practice/extensions/filters_enum_extensions.dart';
 import 'package:practice/pages/new_ping_page.dart';
 import 'package:practice/pages/search_page.dart';
 import 'package:practice/pages/settings_page.dart';
-import 'package:practice/providers/tab_provider.dart';
 
 class HomePage extends ConsumerStatefulWidget {
   const HomePage({super.key});
 
   @override
-  ConsumerState<HomePage> createState() => _SystemHomeState();
+  _HomePageState createState() => _HomePageState();
 }
 
-class _SystemHomeState extends ConsumerState<HomePage> {
-  late final PageController pageController;
-  late final ScrollController settingsController;
-  final focusNode = FocusNode();
+class _HomePageState extends ConsumerState<HomePage>
+    with SingleTickerProviderStateMixin {
+  late TabController _tabController;
 
   @override
   void initState() {
     super.initState();
+    _tabController =
+        TabController(length: FiltersEnum.values.length, vsync: this);
+  }
 
-    pageController = PageController(initialPage: ref.read(tabProvider).index);
-    settingsController = ScrollController();
+  @override
+  void dispose() {
+    _tabController.dispose();
+    super.dispose();
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        body: SafeArea(
-            child: Column(
-      children: [
-        Expanded(
-            child: PageView.builder(
-                itemCount: 4,
-                scrollDirection: Axis.horizontal,
-                controller: pageController,
-                physics: const NeverScrollableScrollPhysics(),
-                allowImplicitScrolling: true,
-                itemBuilder: (_, i) {
-                  switch (i) {
-                    case 1:
-                      return SearchPage();
-                    case 2:
-                      return SettingsPage();
-                    case 3:
-                      return SizedBox.shrink();
-                    case 0:
-                    default:
-                      return ExplorePage();
-                  }
-                })),
-        Row(
-          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-          crossAxisAlignment: CrossAxisAlignment.center,
+      body: SafeArea(
+        child: Column(
           children: [
-            SystemTap(
-                onTap: () => Navigator.push(
-                      context,
-                      MaterialPageRoute(builder: (context) => SearchPage()),
-                    ),
-                child: SizedBox(
-                    height: tapTarget,
-                    width: tapTarget,
-                    child: Center(
+            Expanded(
+              child: DefaultTabController(
+                length: FiltersEnum.values.length,
+                child: NestedScrollView(
+                  headerSliverBuilder:
+                      (BuildContext context, bool innerBoxIsScrolled) {
+                    return <Widget>[
+                      SliverAppBar(
+                        floating: true,
+                        pinned: false,
+                        snap: false,
+                        backgroundColor: Colors
+                            .transparent, // Make the background transparent
+                        toolbarHeight: 0.0, // Remove the unused top area
+                        bottom: TabBar(
+                          controller: _tabController,
+                          indicator: BoxDecoration(),
+                          isScrollable: true,
+                          dividerColor:
+                              Colors.transparent, // Remove the divider color
+                          splashFactory:
+                              NoSplash.splashFactory, // Remove splash effect
+                          labelColor: Theme.of(context).colorScheme.primary,
+                          unselectedLabelColor:
+                              Theme.of(context).colorScheme.secondary,
+                          tabs: FiltersEnum.values.map((filter) {
+                            return Text(filter.title(),
+                                style: TextStyle(fontSize: 28));
+                          }).toList(),
+                        ),
+                      ),
+                    ];
+                  },
+                  body: TabBarView(
+                    controller: _tabController,
+                    children: FiltersEnum.values.map((filter) {
+                      return CustomScrollView(
+                        slivers: [
+                          SliverList(
+                            delegate: SliverChildBuilderDelegate(
+                              (BuildContext context, int index) {
+                                return Container(
+                                  height: 50,
+                                  color: Colors.teal[100 * ((index % 8) + 1)],
+                                  child: Center(child: Text('Item #$index')),
+                                );
+                              },
+                              childCount: 20,
+                            ),
+                          ),
+                        ],
+                      );
+                    }).toList(),
+                  ),
+                ),
+              ),
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                SystemTap(
+                    onTap: () => Navigator.push(
+                          context,
+                          MaterialPageRoute(builder: (context) => SearchPage()),
+                        ),
+                    child: SizedBox(
+                        height: tapTarget,
+                        width: tapTarget,
+                        child: Center(
+                            child: Icon(PhosphorIcons.magnifying_glass,
+                                size: 34)))),
+                SystemTap(
+                    onTap: () => Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                              builder: (context) => NewPingPage()),
+                        ),
+                    child: Icon(PhosphorIcons.circle_fill, size: 72)),
+                SystemTap(
+                    onTap: () => Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                              builder: (context) => SettingsPage()),
+                        ),
+                    child: SizedBox(
+                        height: tapTarget,
+                        width: tapTarget,
                         child:
-                            Icon(PhosphorIcons.magnifying_glass, size: 34)))),
-            SystemTap(
-                onTap: () => Navigator.push(
-                      context,
-                      MaterialPageRoute(builder: (context) => NewPingPage()),
-                    ),
-                child: Icon(PhosphorIcons.circle_fill, size: 72)),
-            SystemTap(
-                onTap: () => Navigator.push(
-                      context,
-                      MaterialPageRoute(builder: (context) => SettingsPage()),
-                    ),
-                child: SizedBox(
-                    height: tapTarget,
-                    width: tapTarget,
-                    child: Center(child: Icon(PhosphorIcons.gear, size: 34)))),
+                            Center(child: Icon(PhosphorIcons.gear, size: 34)))),
+              ],
+            )
           ],
-        )
-      ],
-    )));
+        ),
+      ),
+    );
   }
 }
