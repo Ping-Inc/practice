866bb2ad9dc4560b0fc9b86642780413dc9eeab7
Elliott Etzkorn <etz@wikimedia.org>
2025-03-14 21:40:39 -0400

    Clean up browse page

--- lib/pages/browse_page.dart
+++ lib/pages/browse_page.dart
@@ -1,36 +1,42 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_card_swiper/flutter_card_swiper.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
-import 'package:practice/components/ping_list.dart';
+import 'package:practice/components/ping_focus_list.dart';
+import 'package:practice/components/ping_grid.dart';
+import 'package:practice/constants.dart';
 import 'package:practice/data/ping_data.dart';
+import 'package:practice/design_system/system_loader.dart';
+import 'package:practice/design_system/system_text.dart';
+import 'package:practice/enums/browse_enum.dart';
+import 'package:practice/enums/font_enum.dart';
 import 'package:practice/providers/browse_provider.dart';
+import 'package:practice/providers/browse_temp_provider.dart';
 
-class BrowsePage extends ConsumerStatefulWidget {
+class BrowsePage extends ConsumerWidget {
   const BrowsePage({super.key, required this.asyncPings});
 
   final AsyncValue<List<PingData>> asyncPings;
 
   @override
-  ConsumerState<BrowsePage> createState() => _BrowserPageState();
-}
+  Widget build(BuildContext context, WidgetRef ref) {
+    final BrowseEnum browseMode =
+        ref.watch(browseTempProvider) ?? ref.watch(browseProvider);
 
-class _BrowserPageState extends ConsumerState<BrowsePage> {
-  late final PageController browseController;
-  late final CardSwiperController swipeController;
-  late final ScrollController listController;
-
-  @override
-  void initState() {
-    super.initState();
-
-    browseController =
-        PageController(initialPage: ref.read(browseProvider).index);
-    swipeController = CardSwiperController();
-    listController = ScrollController();
-  }
-
-  @override
-  Widget build(BuildContext contex) {
-    return PingList(asyncPings: widget.asyncPings);
+    return switch (asyncPings) {
+      AsyncData(value: final pingsValue) => pingsValue.isEmpty
+          ? Center(
+              child: SystemText(
+              text: 'No pings match this filter',
+              font: FontEnum.sfmono,
+            ))
+          : browseMode == BrowseEnum.focus
+              ? PingFocusList(pings: pingsValue)
+              : Padding(
+                  padding: EdgeInsets.symmetric(
+                    horizontal: spacingMedium,
+                  ), // Add padding here
+                  child: PingGrid(pings: pingsValue)),
+      AsyncError() => SystemText(text: "Error"),
+      _ => SystemLoader()
+    };
   }
 }
