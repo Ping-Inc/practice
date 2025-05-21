057aebe6aa06baf5fe7a9fd27ced6305f48e8dc0
Rachel Ostrowski <84097695+rpostrowski@users.noreply.github.com>
2025-05-21 13:32:18 -0400

    Resolve packages

--- lib/main.dart
+++ lib/main.dart
@@ -14,7 +14,7 @@ import 'package:practice/utils/backup_utils.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 import 'package:sqflite/sqflite.dart';
 import 'package:flutter_localizations/flutter_localizations.dart';
-import 'package:flutter_gen/gen_l10n/app_localizations.dart';
+import 'package:practice/l10n/app_localizations.dart';
 
 Future<void> setBackupSha() async {
   if (await prefs.getString(sharedPrefsBackupSha) == null) {
