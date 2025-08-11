1eee888acc44f276ec6935a003298d25a5aa7599
Rachel Ostrowski <84097695+rpostrowski@users.noreply.github.com>
2025-08-12 00:18:59 +0100

    Implement cached search provider to reduce flickering

--- lib/pages/settings_page.dart
+++ lib/pages/settings_page.dart
@@ -4,6 +4,7 @@ import 'package:flutter/services.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:practice/components/browse_activation_cell.dart';
 import 'package:practice/components/focus_buttons_settings_cell.dart';
+import 'package:practice/components/cached_search_toggle.dart';
 import 'package:practice/components/cloud_backup_activation_cell.dart';
 import 'package:practice/components/clear_saved_searches_cell.dart';
 import 'package:practice/components/import_pings_button.dart';
@@ -38,6 +39,7 @@ class SettingsPage extends ConsumerWidget {
                         NavCellCluster(text: 'Dev Settings', children: [
                           ClearSavedSearchesCell(),
                           HideFocusButtonsSettingCell(),
+                          CachedSearchToggleCell()
                         ]),
                         SizedBox(
                           height: spacingMedium,
