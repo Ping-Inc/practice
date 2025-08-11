9dfa7d53372ddacb0010cea75cf0ea4f5bd7b3fe
Rachel Ostrowski <84097695+rpostrowski@users.noreply.github.com>
2025-08-12 00:18:59 +0100

    Implement cached search provider to reduce flickering

--- lib/components/ping_search_list.dart
+++ lib/components/ping_search_list.dart
@@ -3,13 +3,19 @@ import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:practice/components/ping_grid.dart';
 import 'package:practice/design_system/system_text.dart';
 import 'package:practice/providers/search_provider.dart';
+import 'package:practice/providers/cached_search_provider.dart';
+import 'package:practice/providers/cached_search_setting_provider.dart';
 
 class PingSearchList extends ConsumerWidget {
   const PingSearchList({super.key});
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
-    final pings = ref.watch(searchProvider);
+    final useCachedSearch = ref.watch(cachedSearchSettingProvider);
+    
+    final pings = useCachedSearch 
+      ? ref.watch(cachedSearchProvider)
+      : ref.watch(searchProvider);
 
     return switch (pings) {
       AsyncData(value: final pingsValue) => PingGrid(pings: pingsValue, shouldRetainScrollPosition: true),
