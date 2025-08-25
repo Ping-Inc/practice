dd10ad57ee5a52b7eba9497da2e106f46f4ed763
Rachel Ostrowski <84097695+rpostrowski@users.noreply.github.com>
2025-08-24 20:20:06 -0700

    Make PingData class abstract to play nice with Flutter

--- lib/data/ping_data.freezed.dart
+++ lib/data/ping_data.freezed.dart
@@ -1,3 +1,4 @@
+// dart format width=80
 // coverage:ignore-file
 // GENERATED CODE - DO NOT MODIFY BY HAND
 // ignore_for_file: type=lint
@@ -9,122 +10,69 @@ part of 'ping_data.dart';
 // FreezedGenerator
 // **************************************************************************
 
+// dart format off
 T _$identity<T>(T value) => value;
-
-final _privateConstructorUsedError = UnsupportedError(
-    'It seems like you constructed your class using `MyClass._()`. This constructor is only meant to be used by freezed and you are not supposed to need it nor use it.\nPlease check the documentation here for more information: https://github.com/rrousselGit/freezed#adding-getters-and-methods-to-our-models');
-
 PingData _$PingDataFromJson(Map<String, dynamic> json) {
   return _Ping.fromJson(json);
 }
 
 /// @nodoc
 mixin _$PingData {
-  int? get id => throw _privateConstructorUsedError;
+  int? get id;
   @DateTimeConverter()
-  DateTime get time => throw _privateConstructorUsedError;
-  String get text => throw _privateConstructorUsedError;
-  int? get replyId => throw _privateConstructorUsedError;
-  int get viewCount => throw _privateConstructorUsedError;
-  int get resonantCount => throw _privateConstructorUsedError;
+  DateTime get time;
+  String get text;
+  int? get replyId;
+  int get viewCount;
+  int get resonantCount;
   @BooleanConverter()
-  bool get hidden => throw _privateConstructorUsedError;
+  bool get hidden;
   @NullableDateTimeConverter()
-  DateTime? get resonantTime => throw _privateConstructorUsedError;
-
-  /// Serializes this PingData to a JSON map.
-  Map<String, dynamic> toJson() => throw _privateConstructorUsedError;
+  DateTime? get resonantTime;
 
   /// Create a copy of PingData
   /// with the given fields replaced by the non-null parameter values.
   @JsonKey(includeFromJson: false, includeToJson: false)
-  $PingDataCopyWith<PingData> get copyWith =>
-      throw _privateConstructorUsedError;
-}
-
-/// @nodoc
-abstract class $PingDataCopyWith<$Res> {
-  factory $PingDataCopyWith(PingData value, $Res Function(PingData) then) =
-      _$PingDataCopyWithImpl<$Res, PingData>;
-  @useResult
-  $Res call(
-      {int? id,
-      @DateTimeConverter() DateTime time,
-      String text,
-      int? replyId,
-      int viewCount,
-      int resonantCount,
-      @BooleanConverter() bool hidden,
-      @NullableDateTimeConverter() DateTime? resonantTime});
-}
-
-/// @nodoc
-class _$PingDataCopyWithImpl<$Res, $Val extends PingData>
-    implements $PingDataCopyWith<$Res> {
-  _$PingDataCopyWithImpl(this._value, this._then);
-
-  // ignore: unused_field
-  final $Val _value;
-  // ignore: unused_field
-  final $Res Function($Val) _then;
-
-  /// Create a copy of PingData
-  /// with the given fields replaced by the non-null parameter values.
   @pragma('vm:prefer-inline')
+  $PingDataCopyWith<PingData> get copyWith =>
+      _$PingDataCopyWithImpl<PingData>(this as PingData, _$identity);
+
+  /// Serializes this PingData to a JSON map.
+  Map<String, dynamic> toJson();
+
   @override
-  $Res call({
-    Object? id = freezed,
-    Object? time = null,
-    Object? text = null,
-    Object? replyId = freezed,
-    Object? viewCount = null,
-    Object? resonantCount = null,
-    Object? hidden = null,
-    Object? resonantTime = freezed,
-  }) {
-    return _then(_value.copyWith(
-      id: freezed == id
-          ? _value.id
-          : id // ignore: cast_nullable_to_non_nullable
-              as int?,
-      time: null == time
-          ? _value.time
-          : time // ignore: cast_nullable_to_non_nullable
-              as DateTime,
-      text: null == text
-          ? _value.text
-          : text // ignore: cast_nullable_to_non_nullable
-              as String,
-      replyId: freezed == replyId
-          ? _value.replyId
-          : replyId // ignore: cast_nullable_to_non_nullable
-              as int?,
-      viewCount: null == viewCount
-          ? _value.viewCount
-          : viewCount // ignore: cast_nullable_to_non_nullable
-              as int,
-      resonantCount: null == resonantCount
-          ? _value.resonantCount
-          : resonantCount // ignore: cast_nullable_to_non_nullable
-              as int,
-      hidden: null == hidden
-          ? _value.hidden
-          : hidden // ignore: cast_nullable_to_non_nullable
-              as bool,
-      resonantTime: freezed == resonantTime
-          ? _value.resonantTime
-          : resonantTime // ignore: cast_nullable_to_non_nullable
-              as DateTime?,
-    ) as $Val);
+  bool operator ==(Object other) {
+    return identical(this, other) ||
+        (other.runtimeType == runtimeType &&
+            other is PingData &&
+            (identical(other.id, id) || other.id == id) &&
+            (identical(other.time, time) || other.time == time) &&
+            (identical(other.text, text) || other.text == text) &&
+            (identical(other.replyId, replyId) || other.replyId == replyId) &&
+            (identical(other.viewCount, viewCount) ||
+                other.viewCount == viewCount) &&
+            (identical(other.resonantCount, resonantCount) ||
+                other.resonantCount == resonantCount) &&
+            (identical(other.hidden, hidden) || other.hidden == hidden) &&
+            (identical(other.resonantTime, resonantTime) ||
+                other.resonantTime == resonantTime));
+  }
+
+  @JsonKey(includeFromJson: false, includeToJson: false)
+  @override
+  int get hashCode => Object.hash(runtimeType, id, time, text, replyId,
+      viewCount, resonantCount, hidden, resonantTime);
+
+  @override
+  String toString() {
+    return 'PingData(id: $id, time: $time, text: $text, replyId: $replyId, viewCount: $viewCount, resonantCount: $resonantCount, hidden: $hidden, resonantTime: $resonantTime)';
   }
 }
 
 /// @nodoc
-abstract class _$$PingImplCopyWith<$Res> implements $PingDataCopyWith<$Res> {
-  factory _$$PingImplCopyWith(
-          _$PingImpl value, $Res Function(_$PingImpl) then) =
-      __$$PingImplCopyWithImpl<$Res>;
-  @override
+abstract mixin class $PingDataCopyWith<$Res> {
+  factory $PingDataCopyWith(PingData value, $Res Function(PingData) _then) =
+      _$PingDataCopyWithImpl;
   @useResult
   $Res call(
       {int? id,
@@ -138,11 +86,11 @@ abstract class _$$PingImplCopyWith<$Res> implements $PingDataCopyWith<$Res> {
 }
 
 /// @nodoc
-class __$$PingImplCopyWithImpl<$Res>
-    extends _$PingDataCopyWithImpl<$Res, _$PingImpl>
-    implements _$$PingImplCopyWith<$Res> {
-  __$$PingImplCopyWithImpl(_$PingImpl _value, $Res Function(_$PingImpl) _then)
-      : super(_value, _then);
+class _$PingDataCopyWithImpl<$Res> implements $PingDataCopyWith<$Res> {
+  _$PingDataCopyWithImpl(this._self, this._then);
+
+  final PingData _self;
+  final $Res Function(PingData) _then;
 
   /// Create a copy of PingData
   /// with the given fields replaced by the non-null parameter values.
@@ -158,37 +106,37 @@ class __$$PingImplCopyWithImpl<$Res>
     Object? hidden = null,
     Object? resonantTime = freezed,
   }) {
-    return _then(_$PingImpl(
+    return _then(_self.copyWith(
       id: freezed == id
-          ? _value.id
+          ? _self.id
           : id // ignore: cast_nullable_to_non_nullable
               as int?,
       time: null == time
-          ? _value.time
+          ? _self.time
           : time // ignore: cast_nullable_to_non_nullable
               as DateTime,
       text: null == text
-          ? _value.text
+          ? _self.text
           : text // ignore: cast_nullable_to_non_nullable
               as String,
       replyId: freezed == replyId
-          ? _value.replyId
+          ? _self.replyId
           : replyId // ignore: cast_nullable_to_non_nullable
               as int?,
       viewCount: null == viewCount
-          ? _value.viewCount
+          ? _self.viewCount
           : viewCount // ignore: cast_nullable_to_non_nullable
               as int,
       resonantCount: null == resonantCount
-          ? _value.resonantCount
+          ? _self.resonantCount
           : resonantCount // ignore: cast_nullable_to_non_nullable
               as int,
       hidden: null == hidden
-          ? _value.hidden
+          ? _self.hidden
           : hidden // ignore: cast_nullable_to_non_nullable
               as bool,
       resonantTime: freezed == resonantTime
-          ? _value.resonantTime
+          ? _self.resonantTime
           : resonantTime // ignore: cast_nullable_to_non_nullable
               as DateTime?,
     ));
@@ -198,8 +146,8 @@ class __$$PingImplCopyWithImpl<$Res>
 /// @nodoc
 
 @JsonSerializable(fieldRename: FieldRename.snake)
-class _$PingImpl implements _Ping {
-  const _$PingImpl(
+class _Ping implements PingData {
+  const _Ping(
       {this.id,
       @DateTimeConverter() required this.time,
       required this.text,
@@ -208,9 +156,7 @@ class _$PingImpl implements _Ping {
       required this.resonantCount,
       @BooleanConverter() required this.hidden,
       @NullableDateTimeConverter() required this.resonantTime});
-
-  factory _$PingImpl.fromJson(Map<String, dynamic> json) =>
-      _$$PingImplFromJson(json);
+  factory _Ping.fromJson(Map<String, dynamic> json) => _$PingFromJson(json);
 
   @override
   final int? id;
@@ -232,16 +178,26 @@ class _$PingImpl implements _Ping {
   @NullableDateTimeConverter()
   final DateTime? resonantTime;
 
+  /// Create a copy of PingData
+  /// with the given fields replaced by the non-null parameter values.
   @override
-  String toString() {
-    return 'PingData(id: $id, time: $time, text: $text, replyId: $replyId, viewCount: $viewCount, resonantCount: $resonantCount, hidden: $hidden, resonantTime: $resonantTime)';
+  @JsonKey(includeFromJson: false, includeToJson: false)
+  @pragma('vm:prefer-inline')
+  _$PingCopyWith<_Ping> get copyWith =>
+      __$PingCopyWithImpl<_Ping>(this, _$identity);
+
+  @override
+  Map<String, dynamic> toJson() {
+    return _$PingToJson(
+      this,
+    );
   }
 
   @override
   bool operator ==(Object other) {
     return identical(this, other) ||
         (other.runtimeType == runtimeType &&
-            other is _$PingImpl &&
+            other is _Ping &&
             (identical(other.id, id) || other.id == id) &&
             (identical(other.time, time) || other.time == time) &&
             (identical(other.text, text) || other.text == text) &&
@@ -260,60 +216,85 @@ class _$PingImpl implements _Ping {
   int get hashCode => Object.hash(runtimeType, id, time, text, replyId,
       viewCount, resonantCount, hidden, resonantTime);
 
-  /// Create a copy of PingData
-  /// with the given fields replaced by the non-null parameter values.
-  @JsonKey(includeFromJson: false, includeToJson: false)
   @override
-  @pragma('vm:prefer-inline')
-  _$$PingImplCopyWith<_$PingImpl> get copyWith =>
-      __$$PingImplCopyWithImpl<_$PingImpl>(this, _$identity);
-
-  @override
-  Map<String, dynamic> toJson() {
-    return _$$PingImplToJson(
-      this,
-    );
+  String toString() {
+    return 'PingData(id: $id, time: $time, text: $text, replyId: $replyId, viewCount: $viewCount, resonantCount: $resonantCount, hidden: $hidden, resonantTime: $resonantTime)';
   }
 }
 
-abstract class _Ping implements PingData {
-  const factory _Ping(
-          {final int? id,
-          @DateTimeConverter() required final DateTime time,
-          required final String text,
-          final int? replyId,
-          required final int viewCount,
-          required final int resonantCount,
-          @BooleanConverter() required final bool hidden,
-          @NullableDateTimeConverter() required final DateTime? resonantTime}) =
-      _$PingImpl;
+/// @nodoc
+abstract mixin class _$PingCopyWith<$Res> implements $PingDataCopyWith<$Res> {
+  factory _$PingCopyWith(_Ping value, $Res Function(_Ping) _then) =
+      __$PingCopyWithImpl;
+  @override
+  @useResult
+  $Res call(
+      {int? id,
+      @DateTimeConverter() DateTime time,
+      String text,
+      int? replyId,
+      int viewCount,
+      int resonantCount,
+      @BooleanConverter() bool hidden,
+      @NullableDateTimeConverter() DateTime? resonantTime});
+}
 
-  factory _Ping.fromJson(Map<String, dynamic> json) = _$PingImpl.fromJson;
+/// @nodoc
+class __$PingCopyWithImpl<$Res> implements _$PingCopyWith<$Res> {
+  __$PingCopyWithImpl(this._self, this._then);
 
-  @override
-  int? get id;
-  @override
-  @DateTimeConverter()
-  DateTime get time;
-  @override
-  String get text;
-  @override
-  int? get replyId;
-  @override
-  int get viewCount;
-  @override
-  int get resonantCount;
-  @override
-  @BooleanConverter()
-  bool get hidden;
-  @override
-  @NullableDateTimeConverter()
-  DateTime? get resonantTime;
+  final _Ping _self;
+  final $Res Function(_Ping) _then;
 
   /// Create a copy of PingData
   /// with the given fields replaced by the non-null parameter values.
   @override
-  @JsonKey(includeFromJson: false, includeToJson: false)
-  _$$PingImplCopyWith<_$PingImpl> get copyWith =>
-      throw _privateConstructorUsedError;
+  @pragma('vm:prefer-inline')
+  $Res call({
+    Object? id = freezed,
+    Object? time = null,
+    Object? text = null,
+    Object? replyId = freezed,
+    Object? viewCount = null,
+    Object? resonantCount = null,
+    Object? hidden = null,
+    Object? resonantTime = freezed,
+  }) {
+    return _then(_Ping(
+      id: freezed == id
+          ? _self.id
+          : id // ignore: cast_nullable_to_non_nullable
+              as int?,
+      time: null == time
+          ? _self.time
+          : time // ignore: cast_nullable_to_non_nullable
+              as DateTime,
+      text: null == text
+          ? _self.text
+          : text // ignore: cast_nullable_to_non_nullable
+              as String,
+      replyId: freezed == replyId
+          ? _self.replyId
+          : replyId // ignore: cast_nullable_to_non_nullable
+              as int?,
+      viewCount: null == viewCount
+          ? _self.viewCount
+          : viewCount // ignore: cast_nullable_to_non_nullable
+              as int,
+      resonantCount: null == resonantCount
+          ? _self.resonantCount
+          : resonantCount // ignore: cast_nullable_to_non_nullable
+              as int,
+      hidden: null == hidden
+          ? _self.hidden
+          : hidden // ignore: cast_nullable_to_non_nullable
+              as bool,
+      resonantTime: freezed == resonantTime
+          ? _self.resonantTime
+          : resonantTime // ignore: cast_nullable_to_non_nullable
+              as DateTime?,
+    ));
+  }
 }
+
+// dart format on
